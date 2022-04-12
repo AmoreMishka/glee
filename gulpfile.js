3be1742abfbbd225b3261e8de7d93cdf48f65141
AmoreMishka <bogachenkoana@gmail.com>
2022-04-12 15:11:34 +0300

    start header

--- gulpfile.js
+++ gulpfile.js
@@ -107,15 +107,3 @@ exports.cleanDist = cleanDist;
 
 exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
-
-
-
-
-
-
-
-
-
-// function (){
-//     return
-// }
